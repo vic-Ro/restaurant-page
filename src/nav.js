4b835f836e3363ed7123436458820ac8cb023348
Vic Romero <vromerocomino@gmail.com>
2020-11-25 19:08:30 +0100

    1.0 Nav finished

---
+++ src/nav.js
@@ -0,0 +1,28 @@
+const generateNav = () => {
+  const nav = document.getElementById('nav');
+  const ul = document.createElement('ul');
+  ul.classList.add('main-menu');
+  const fragment =  document.createDocumentFragment();
+  for (let i = 0; i < 3; i += 1) {
+    const li = document.createElement('li');
+    li.classList.add('main-menu__item');
+    switch (i) {
+      case 0:
+        li.textContent = 'HOME';
+        break;
+      case 1:
+        li.textContent = 'MENU';
+        break;
+      case 2:
+        li.textContent = 'CONTACT';
+        break;
+      default:
+        break;
+    }
+    fragment.appendChild(li);
+  }
+  ul.appendChild(fragment);
+  nav.appendChild(ul);
+}
+
+export default generateNav;
